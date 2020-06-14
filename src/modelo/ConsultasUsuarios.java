b1b3b3c9f59d42a877aa6110a55b267144567daf
cerso <vicecers@gmail.com>
2020-06-14 11:44:12 -0300

    Se creo el metodo Registrar en la clase ConsultaUsusrios

--- src/modelo/ConsultasUsuarios.java
+++ src/modelo/ConsultasUsuarios.java
@@ -1,4 +1,4 @@
- /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -15,8 +15,27 @@ import java.util.logging.Logger;
  */
 public class ConsultasUsuarios extends Conexion {
 
-     
+    public void Registrar(Usuarios user) {
+        Connection cn = getConexion();
+        PreparedStatement ps = null;
+        String sql = "INSERT INTO `user` VALUES(?,?, ?, ?)";
+        
+        try {
+            ps = cn.prepareStatement(sql);
+            
+            ps.setInt(1, user.getUser_id());
+            ps.setString(2, user.getUser_account());
+            ps.setString(3, user.getUser_pass());
+            ps.setInt(4, user.getUser_type());
+            
+            ps.execute();
+            
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al registrar usuario " + ex.toString());
+        }
+        
 
-
+    }
 
 }
